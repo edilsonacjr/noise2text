e15b289107f45a45a8755801789114c1488304d7
Edilson <edilsonacjr@gmail.com>
2020-06-23 19:15:49 +0200

    correcting paths in get_data

--- get_data.py
+++ get_data.py
@@ -21,13 +21,13 @@ BASE_NILC_URL = 'http://143.107.183.175:22980/download.php?file=embeddings/'
 
 class NILCDriver:
     def __init__(self, emb_type='word2vec', subtype='skip', dimension=100, output_path='embeddings',
-                 output_pattern='/{emb_type}_{subtype}_{dimension}', output_temp='emb_tmp', force_download=False):
+                 output_pattern='{emb_type}_{subtype}_{dimension}', output_temp='emb_tmp', force_download=False):
         self.emb_type = emb_type
         self.subtype = subtype
         self.dimension = dimension
         self.file_name = output_pattern.format(emb_type=emb_type, subtype=subtype, dimension=dimension)
         self.output_save = os.path.join(output_path)
-        self.output_tmp = os.path.join(output_path, output_temp)
+        self.output_tmp = os.path.join(output_path, output_temp, self.file_name)
         self.force_download = force_download
 
         if self.dimension not in [50, 100, 300, 600, 1000]:
@@ -44,18 +44,18 @@ class NILCDriver:
             raise ValueError('Invalid emb_type, try "word2vec", "glove", "wang2vec" or "fasttext".')
 
     def download(self):
-        if not os.path.exists(self.output_tmp + self.file_name + '.zip') or self.force_download:
+        if not os.path.exists(self.output_tmp + '.zip') or self.force_download:
             response = urllib.request.urlopen(self.URL)
             data = response.read()
             if not os.path.exists(self.output_tmp):
                 os.makedirs(self.output_tmp)
-            with open(self.output_tmp + self.file_name + '.zip', 'wb') as emb_file:
+            with open(self.output_tmp + '.zip', 'wb') as emb_file:
                 emb_file.write(data)
         else:
             print('Embedding already downloaded, if you want to download again please use force_download')
 
     def extract(self):
-        with ZipFile(self.output_tmp + self.file_name + '.zip', 'r') as zipdata:
+        with ZipFile(self.output_tmp + '.zip', 'r') as zipdata:
             # Extract all the contents of zip file in current directory
             zip_info = zipdata.infolist()[0]
             zip_info.filename = self.file_name + '.txt'
